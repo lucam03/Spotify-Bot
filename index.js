5171b20273c4d677cdc93a4ab5a30bf657d8eeef
lucam03 <62672581+lucam03@users.noreply.github.com>
2020-07-07 23:43:54 +0100

    mbee

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ client.on("ready", () => {
 client.on("message", (msg) => {
   let msgData = msg.content.split()
   if (msgData[0] == "$np" && msg.member.presence.activities[0]) {
+    let activity = msg.member.presence.activities[0];
     if (msg.member.presence.activities[0].name == "Spotify") {
-      let activity = msg.member.presence.activities[0];
       let songTitle = activity.details;
       let albumTitle = activity.assets.largeText;
       let artistName = activity.state;
+      let textToSend = msg.member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
+      msg.channel.send(textToSend);
+    } else if (msg.member.presence.activities[0].name == "MusicBee") {
+      let songTitle = activity.state;
+      let albumTitle = activity.details.substr(activity.details.indexOf("-")+2);
+      let artistName = activity.details.split(" - ")[0];
       let textToSend = msg.member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
       msg.channel.send(textToSend);
     }
   } else if (msgData[0] == "$np") {
     msg.channel.send("Not playing anything");
   }
-})
+});
