c1904b3ce9ffb0b33184f74fd338240af5f107fd
lucam03 <62672581+lucam03@users.noreply.github.com>
2020-07-08 00:11:45 +0100

    crap embed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, MessageEmbed }= require("discord.js");
+const client = new Client();
 
 const TOKEN = "TOKENHERE";
 client.login(TOKEN);
@@ -17,15 +17,35 @@ client.on("message", (msg) => {
       let albumTitle = activity.assets.largeText;
       let artistName = activity.state;
       let textToSend = msg.member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
-      msg.channel.send(textToSend);
+      msg.channel.send(makeEmbed(msg.member.displayName, songTitle, albumTitle, artistName));
     } else if (msg.member.presence.activities[0].name == "MusicBee") {
       let songTitle = activity.state;
       let albumTitle = activity.details.substr(activity.details.indexOf("-")+2);
       let artistName = activity.details.split(" - ")[0];
       let textToSend = msg.member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
-      msg.channel.send(textToSend);
+      msg.channel.send(makeEmbed(msg.member.displayName, songTitle, albumTitle, artistName));
     }
   } else if (msgData[0] == "$np") {
     msg.channel.send("Not playing anything");
   }
 });
+
+function makeEmbed(nick, track, album, artist, ) {
+  let embed = new MessageEmbed()
+  .setTitle("Now playing for " + nick)
+  .addField("Song:", track)
+  .addField("Album:", album)
+  .addField("Artist:", artist)
+  .setColor(randomHex());
+  return embed;
+}
+
+function randomHex() {
+  let r = Math.floor(256*Math.random()).toString(16);
+  let g = Math.floor(256*Math.random()).toString(16);
+  let b = Math.floor(256*Math.random()).toString(16);
+  if (r.length == 1) {r = "0" + r};
+  if (g.length == 1) {g = "0" + g};
+  if (b.length == 1) {b = "0" + b};
+  return "#" + r + g + b;
+}
