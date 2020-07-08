96a5116c67be1480e333200e7525099914fd80b4
lucam03 <62672581+lucam03@users.noreply.github.com>
2020-07-08 23:07:39 +0100

    refactor, allow for mention other users

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const keepAlive = require('./server');
+
 const { Client, MessageEmbed }= require("discord.js");
 const client = new Client();
 
+keepAlive();
 const TOKEN = "TOKENHERE";
 client.login(TOKEN);
 
@@ -9,24 +12,31 @@ client.on("ready", () => {
 })
 
 client.on("message", (msg) => {
-  let msgData = msg.content.split()
-  if (msgData[0] == "$np" && msg.member.presence.activities[0]) {
-    let activity = msg.member.presence.activities[0];
-    if (msg.member.presence.activities[0].name == "Spotify") {
-      let songTitle = activity.details;
-      let albumTitle = activity.assets.largeText;
-      let artistName = activity.state;
-      let textToSend = msg.member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
-      msg.channel.send(makeEmbed(msg.member.displayName, songTitle, albumTitle, artistName));
-    } else if (msg.member.presence.activities[0].name == "MusicBee") {
-      let songTitle = activity.state;
-      let albumTitle = activity.details.substr(activity.details.indexOf("-")+2);
-      let artistName = activity.details.split(" - ")[0];
-      let textToSend = msg.member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
-      msg.channel.send(makeEmbed(msg.member.displayName, songTitle, albumTitle, artistName));
+  let msgData = msg.content.split(" ");
+  if (msgData[0] == "$np") {
+    let member = msg.mentions.members.size ? msg.mentions.members.first() : msg.member;
+    if (member.presence.activities[0]) {
+      for (let i = 0; i < member.presence.activities.length; i++) {
+        let activity = member.presence.activities[i]
+        if (activity.name == "Spotify") {
+          let songTitle = activity.details;
+          let albumTitle = activity.assets.largeText;
+          let artistName = activity.state;
+          let textToSend = member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
+          msg.channel.send(makeEmbed(member.displayName, songTitle, albumTitle, artistName));
+          break;
+        } else if (activity.name == "MusicBee") {
+          let songTitle = activity.state;
+          let albumTitle = activity.details.substr(activity.details.indexOf("-")+2);
+          let artistName = activity.details.split(" - ")[0];
+          let textToSend = member.displayName + " is now playing **"+ songTitle + "** by **" + artistName + "** on **" + albumTitle + "**";
+          msg.channel.send(makeEmbed(member.displayName, songTitle, albumTitle, artistName));
+          break;
+        }
+      }
+    } else {
+      msg.channel.send("Not playing anything");
     }
-  } else if (msgData[0] == "$np") {
-    msg.channel.send("Not playing anything");
   }
 });
 
@@ -38,7 +48,7 @@ function makeEmbed(nick, track, album, artist, ) {
   .addField("Artist:", artist)
   .setColor(randomHex());
   return embed;
-}
+};
 
 function randomHex() {
   let r = Math.floor(256*Math.random()).toString(16);
@@ -48,4 +58,4 @@ function randomHex() {
   if (g.length == 1) {g = "0" + g};
   if (b.length == 1) {b = "0" + b};
   return "#" + r + g + b;
-}
+};
