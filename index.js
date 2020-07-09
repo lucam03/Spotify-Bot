b81d1db7f84fdfe92ef171aea28c4291455b92ae
lucam03 <62672581+lucam03@users.noreply.github.com>
2020-07-09 18:36:56 +0100

    added support for mentions and nickname id

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Client, MessageEmbed }= require("discord.js");
 const client = new Client();
 
+const fs = require("fs");
+var nickNames = require("./nickNames.json");
+
 const config = require("./config.json");
 const prefix = config.prefix;
 
@@ -15,6 +18,11 @@ client.on("message", (msg) => {
   let msgData = msg.content.split(" ");
   if (msgData[0] == `${prefix}np`) {
     let member = msg.mentions.members.size ? msg.mentions.members.first() : msg.member;
+    if (msgData[1]) {
+      if (userFromNickName(msg.content.substr(msg.content.indexOf(" ")+1), msg.guild)) {
+        member = userFromNickName(msg.content.substr(msg.content.indexOf(" ")+1), msg.guild);
+      }
+    };
     if (member.presence.activities[0]) {
       for (let i = 0; i < member.presence.activities.length; i++) {
         let activity = member.presence.activities[i]
@@ -38,9 +46,27 @@ client.on("message", (msg) => {
       msg.channel.send("Not playing anything");
     }
   }
+  if (msgData[0] == `${prefix}updateNicknames`) {
+    nickNames[msg.guild.id] = {};
+    msg.guild.members.cache.each((member) => updateNickName(member, member));
+    msg.channel.send("Updated nicknames");
+  };
 });
 
-function makeEmbed(nick, track, album, artist, ) {
+client.on("guildMemberUpdate", (oldM, newM) => {
+  updateNickName(oldM, newM);
+});
+
+client.on("guildMemberAdd", (member) => {
+  updateNickName(member, member);
+});
+
+client.on("guildCreate", (guild) => {
+  nickNames[guild.id] = {};
+  guild.members.cache.each((member) => updateNickName(member, member));
+});
+
+function makeEmbed(nick, track, album, artist) {
   let embed = new MessageEmbed()
   .setTitle("Now playing for " + nick)
   .addField("Song:", track)
@@ -59,3 +85,30 @@ function randomHex() {
   if (b.length == 1) {b = "0" + b};
   return "#" + r + g + b;
 };
+
+function userFromNickName(nick, guild) {
+  if (!nickNames[guild.id]) {
+    nickNames[guild.id] = {};
+  }
+  return guild.members.cache.get(nickNames[guild.id][nick.toLowerCase()]);
+};
+
+function updateNickName(oldMember, newMember) {
+  if (!nickNames[oldMember.guild.id]) {
+    nickNames[oldMember.guild.id] = {};
+  }
+  nickNames[oldMember.guild.id][oldMember.displayName.toLowerCase()] = null;
+  nickNames[newMember.guild.id][newMember.displayName.toLowerCase()] = newMember.id;
+}
+
+function updateNickNameFile() {
+  fs.writeFile("nickNames.json", JSON.stringify(nickNames), "utf8", (err) => {
+    if (err) {
+      console.log(err)
+      }
+    }
+  );
+  console.log("Updated nickname file");
+}
+
+setInterval(updateNickNameFile, 300000);
